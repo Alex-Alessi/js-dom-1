c3d3b2f0c4b3a603c8805bc1a3d7e85644f4c524
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-23 10:42:07 +0200

    complete

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const button = document.getElementById("change-image");
 const accesa = document.getElementById("accesa");
 const spenta = document.getElementById("spenta");
+let valAccesa = false;
 
 button.addEventListener("click", () => {
-  if (spenta.classList.contains("d-none")) {
-    accesa.classList.remove("d-block");
-    spenta.classList.remove("d-none");
-  } else {
+  if (!valAccesa) {
+    // spenta.classList.add("d-block");
+    accesa.classList.remove("d-none");
     spenta.classList.add("d-none");
-    accesa.classList.add("d-block");
+    valAccesa = true;
+  } else {
+    spenta.classList.remove("d-none");
+    accesa.classList.add("d-none");
+    valAccesa = false;
+    // accesa.classList.add("d-block");
   }
 });
